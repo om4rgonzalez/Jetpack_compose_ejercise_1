3eeb0d1328d325840afd19c0c66118903261838e
Omar Gonzalez <U632163@T0003560570.local>
2023-03-02 17:05:02 -0300

    Adding textField component example

--- app/src/main/java/net/bintelligence/jetpackcomposecatalog/MainActivity.kt
+++ app/src/main/java/net/bintelligence/jetpackcomposecatalog/MainActivity.kt
@@ -13,20 +13,30 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import net.bintelligence.jetpackcomposecatalog.ui.theme.JetPackComposeCatalogTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,23 +49,100 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MyStateSample()
+                    Column() {
+                        MyTextFieldOutlined()
+                    }
                 }
             }
         }
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun MyTextFieldOutlined() {
+    var myText by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = myText,
+        onValueChange = { myText = it },
+        label = { Text(text = "Escribe tu nombre") },
+        modifier = Modifier.padding(24.dp),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Magenta,
+            unfocusedBorderColor = Color.Blue
+        )
+    )
+}
+
+@Composable
+fun MyTextFieldAdvance() {
+    var myText by remember { mutableStateOf("") }
+
+    TextField(
+        value = myText,
+        onValueChange = {
+            myText =
+                if (it.contains("a")) {
+                    it.replace("a", "")
+                } else {
+                    it
+                }
+        },
+        label = { Text(text = "Escribe tu nombre") }
+    )
+}
+
+@Composable
+fun MyTextField() {
+    var myText by remember { mutableStateOf("Marcelo") }
+    Column() {
+
+    }
+    TextField(value = myText, onValueChange = { myText = it })
+}
+
+
+@Composable
+fun MyText() {
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "Esto es un ejemplo")
+        Text(text = "Esto es un ejemplo", color = Color.Blue)
+        Text(text = "Esto es un ejemplo", fontWeight = FontWeight.ExtraBold)
+        Text(text = "Esto es un ejemplo", fontWeight = FontWeight.Light)
+        Text(text = "Esto es un ejemplo", style = TextStyle(fontFamily = FontFamily.Cursive))
+        Text(text = "Esto es un ejemplo", fontFamily = FontFamily.Cursive)
+        Text(
+            text = "Esto es un ejemplo",
+            style = TextStyle(textDecoration = TextDecoration.LineThrough)
+        )
+        Text(
+            text = "Esto es un ejemplo",
+            style = TextStyle(textDecoration = TextDecoration.Underline)
+        )
+        Text(
+            text = "Esto es un ejemplo", style = TextStyle(
+                textDecoration = TextDecoration.combine(
+                    listOf(TextDecoration.Underline, TextDecoration.LineThrough)
+                )
+            )
+        )
+        Text(text = "Esto es un ejemplo", fontSize = 30.sp)
+    }
+
+}
+
 @Composable
 fun MyStateSample() {
-    var counter by rememberSaveable {mutableStateOf(0)}
+    var counter by rememberSaveable { mutableStateOf(0) }
 
     Column(
         Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
-        Button(onClick = { counter += 1}) {
+        Button(onClick = { counter += 1 }) {
             Text(text = "Pulsar")
         }
         Text(text = "He sido pulsado ${counter} veces")
@@ -77,19 +164,22 @@ fun MyColumn() {
         Row(
             Modifier
                 .fillMaxWidth()
-                .weight(1f)) {
+                .weight(1f)
+        ) {
             Box(
                 Modifier
                     .weight(1f)
                     .fillMaxHeight()
-                    .background(Color.Red), contentAlignment = Alignment.Center) {
+                    .background(Color.Red), contentAlignment = Alignment.Center
+            ) {
                 Text(text = "Ejemplo 2")
             }
             Box(
                 Modifier
                     .weight(1f)
                     .fillMaxHeight()
-                    .background(Color.Green), contentAlignment = Alignment.Center) {
+                    .background(Color.Green), contentAlignment = Alignment.Center
+            ) {
                 Text(text = "Ejemplo 3")
             }
         }
@@ -110,7 +200,6 @@ fun MySpacer(size: Int) {
     Spacer(modifier = Modifier.height(size.dp))
 }
 
-@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetPackComposeCatalogTheme {
